6aa045a1ccaef21b319ced07137f2bea0469ae4d
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2018-05-27 22:26:16 +1000

    Include accession lengths

--- annotation_table/annotation_table_extend_gene_details.py
+++ annotation_table/annotation_table_extend_gene_details.py
@@ -23,12 +23,12 @@ def uniref_xml_parse(tableFile, xmlFile):
                                 for entry in line[2].split('['):
                                         accDict[entry.rstrip('] ').split('_')[0]] = ''               # This provides a structure we can quickly check to see if we should hold onto results, and then replace its value with the xml string.
         # Parse through the xml file now and hold onto xml blocks as string values
-        xmlBlock = ['','','']      # Format is [accession, gene name, common taxon]
+        xmlBlock = ['','','','']      # Format is [accession, gene name, common taxon, sequence length]
         with open(xmlFile, 'r') as fileIn:
                 for line in fileIn:
                         # Handle entry lines
                         if line.startswith('<entry id='):
-                                if xmlBlock == ['','', '']:                                     # i.e., if this is the first loop, just start building the current block
+                                if xmlBlock == ['','', '', '']:                                 # i.e., if this is the first loop, just start building the current block
                                         acc = line.split('"')[1].split('_', maxsplit=1)[1]      # This corresponds to the "UniRef###_acc" value
                                         xmlBlock[0] = acc
                                 else:
@@ -38,10 +38,15 @@ def uniref_xml_parse(tableFile, xmlFile):
                                                         xmlBlock[1] = 'None'
                                                 if xmlBlock[2] == '':
                                                         xmlBlock[2] = 'None'
-                                                accDict[xmlBlock[0]] = xmlBlock[1:]             # We just need to hold onto the gene name and taxon code
+                                                # Script failure check
+                                                if xmlBlock[3] == '':
+                                                        print('Something went wrong with XML parsing - couldn\'t find sequence length?')
+                                                        print(xmlBlock)
+                                                        quit()
+                                                accDict[xmlBlock[0]] = xmlBlock[1:]             # We just need to hold onto the gene name, taxon code, and sequence length; the accession becomes the dictionary key
                                         # Start new block
                                         acc = line.split('"')[1].split('_', maxsplit=1)[1]
-                                        xmlBlock = [acc, '', '']
+                                        xmlBlock = [acc, '', '', '']
                         # Handle relevant information lines
                         elif line.startswith('<name>'):
                                 name = line.replace('<name>Cluster: ', '').replace('</name>\n', '')
@@ -49,6 +54,9 @@ def uniref_xml_parse(tableFile, xmlFile):
                         elif line.startswith('<property type="common taxon ID"'):
                                 taxon = line.split('=')[2].strip('"/>\n')
                                 xmlBlock[2] = taxon
+                        elif '<sequence length="' in line:
+                                length = line.split('"')[1]
+                                xmlBlock[3] = length
         return accDict
 
 def validate_args(args):
@@ -92,14 +100,14 @@ with open(args.inputTable, 'r') as fileIn, open(args.outputFileName, 'w') as fil
         for line in fileIn:
                 # Change the header line
                 if line.startswith('#Query\tSource'):
-                        fileOut.write('#Query\tSource\tTarget_accessions\tGene_names\tNCBI_taxonomy_of_hits\tPercentage_identity\tAlignment_length\tMismatches\tGap_opens\tQuery_start\tQuery_end\tTarget_start\tTarget_end\tExpect_value\tBit_score\tBest_hit_with_idmapping\n')
+                        fileOut.write('#Query\tSource\tTarget_accessions\tGene_names\tNCBI_taxonomy_of_hits\tLength_of_accession_seqs\tPercentage_identity\tAlignment_length\tMismatches\tGap_opens\tQuery_start\tQuery_end\tTarget_start\tTarget_end\tExpect_value\tBit_score\tBest_hit_with_idmapping\n')
                 elif line.startswith('#'):
                         continue
                 else:
                         line = line.rstrip('\r\n').split('\t')
                         # If this entry had no hits, just provide an updated blank output line
                         if line[2] == '.':
-                                newL = [*line[0:3], '.', '.', *line[3:]]
+                                newL = [*line[0:3], '.', '.', '.', *line[3:]]
                                 fileOut.write('\t'.join(newL) + '\n')
                         else:
                                 # Parse accessions from the table file's column
@@ -107,16 +115,19 @@ with open(args.inputTable, 'r') as fileIn, open(args.outputFileName, 'w') as fil
                                 # Get and format details
                                 formattedNames = ''
                                 formattedTaxa = ''
+                                formattedLengths = ''
                                 for i in range(len(accs)):
                                         accDetails = accDict[accs[i]]   # Format is [gene name, common taxon]
                                         if i == 0:
                                                 formattedNames += accDetails[0] + ' '
                                                 formattedTaxa += accDetails[1] + ' '
+                                                formattedLengths += accDetails[2] + ' '
                                         else:
                                                 formattedNames += '[' + accDetails[0] + ']'
                                                 formattedTaxa += '[' + accDetails[1] + ']'
+                                                formattedLengths += '[' + accDetails[2] + ']'
                                 # Output
-                                newL = [*line[0:3], formattedNames, formattedTaxa, *line[3:]]
+                                newL = [*line[0:3], formattedNames, formattedTaxa, formattedLengths, *line[3:]]
                                 fileOut.write('\t'.join(newL) + '\n')
 
 # Done!
