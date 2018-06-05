8c83f097afa04922e9dfe8c5d9dda5892070b939
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2018-06-05 18:05:57 +1000

    Select behaviour to retrieve or remove
    Find myself in need of the ability to retrieve entries rather than remove. Easy to implement this behaviour into one script.

--- gff3_retrieve_entries.py
+++ gff3_retrieve_entries.py
@@ -1,50 +1,54 @@
 #! python3
-# gff3_remove_entries.py
-# Program to remove lines from a gff3 formatted file that correspond to
-# entries with IDs provided in a text file. Will also remove PASA
-# comment lines and empty lines to provide a clean and concise file
+# gff3_retrieve_entries.py
+# Program to retrieve or remove lines from a gff3 formatted file that 
+# correspond to entries with IDs provided in a text file.
 
-import os, argparse, re, time
-from Bio import SeqIO
+import os, argparse, re
+
+# Define functions for later use
+def validate_args(args):
+        # Validate input file locations
+        if not os.path.isfile(args.gff3File):
+                print('I am unable to locate the input gff3 file (' + args.gff3File + ')')
+                print('Make sure you\'ve typed the file name or location correctly and try again.')
+                quit()
+        if not os.path.isfile(args.idFile):
+                print('I am unable to locate the query FASTA file (' + args.inputQuery + ')')
+                print('Make sure you\'ve typed the file name or location correctly and try again.')
+                quit()
+        # Handle file overwrites
+        if os.path.isfile(args.outputFileName):
+                print(args.outputFileName + ' already exists. Delete/move/rename this file and run the program again.')
+                quit()
+
+# Established regex for later use
+idRegex = re.compile(r'(evm\.(model|TU)\..+?\.\d{1,10})')
 
 ##### USER INPUT SECTION
 
 usage = """%(prog)s reads in a gff3 file and a user provided list of transcript IDs
-and removes entries present in the ID list. Further cleaning is undertaken to produce more compact files
-without blank lines. The input file and IDs are assumed to result from the EVM -> PASA pipeline, and so
-entry transcript IDs should look like 'evm.model.${CONTIG}.\d+' and its corresponding gene ID would replace 'model'
-with 'TU'
+and produces an output file that either 1) contains entries present in the ID list, or
+2) does NOT contain entries present in the ID list. The input file and IDs are assumed 
+to result from the EVM -> PASA pipeline, and so entry transcript IDs should look 
+like 'evm.model.${CONTIG}.\d+' and its corresponding gene ID would replace 'model'
+with 'TU'.
 """
 p = argparse.ArgumentParser(description=usage)
-p.add_argument("-g", "-gff3", dest="gffFile",
-                  help="Specify gff file")
-p.add_argument("-i", "-idfile", dest="idfile",
-                  help="Specify the text file containing a list of gene IDs (not transcript IDs)")
+p.add_argument("-g", "-gff3", dest="gff3File",
+               help="Specify gff file")
+p.add_argument("-i", "-idfile", dest="idFile",
+               help="Specify the text file containing a list of gene IDs (not transcript IDs)")
+p.add_argument("-b", "-behaviour", dest="behaviour", choices=['retrieve', 'remove'],
+               help="Specify program behaviour to either 'retrieve' lines that contain IDs, or 'remove' lines that contain IDs")
 p.add_argument("-o", "-output", dest="outputFile",
                help="Output file name [text file containing the sequence IDs that match overlap criteria")
-p.add_argument("-f", "-force", dest="force", choices = ['y', 'n', 'Y', 'N'],
-               help="default == 'n', which means the program will not overwrite existing files. Specify 'y' to allow this behaviour at your own risk.", default='n')
-
 args = p.parse_args()
-
-# Obtain data from arguments
-gffFile = args.gffFile
-idfile = args.idfile
-outputFileName = args.outputFile
-force = args.force
-
-# Format output names and check that output won't overwrite another file
-if os.path.isfile(outputFileName) and force.lower() != 'y':
-        print('There is already a file named ' + outputFileName + '. Either specify a new file name, delete these older file(s), or provide the -force argument either "Y" or "y"')
-        quit()
-elif os.path.isfile(outputFileName) and force.lower() == 'y':
-        os.remove(outputFileName)
 
 ### CORE PROCESS
 
 # Parse ID file
 idList = []
-with open(idfile, 'r') as fileIn:
+with open(args.idfile, 'r') as fileIn:
         for line in fileIn:
                 if line == '\n':
                         continue
@@ -52,15 +56,21 @@ with open(idfile, 'r') as fileIn:
                 idList.append(line.replace('model', 'TU').rstrip('\n'))         # This is the gene ID equivalent of the transcript
 
 # Read through the gff3 file and remove entries + clean the file
-idRegex = re.compile(r'(evm\.(model|TU)\..+?\.\d{1,10})')
-with open(gffFile, 'r') as fileIn, open(outputFileName, 'w') as fileOut:
+with open(args.gff3File, 'r') as fileIn, open(args.outputFileName, 'w') as fileOut:
         for line in fileIn:
                 # Skip filler lines
                 if line == '\n':
                         continue
                 # Handle information-containing lines
                 modelID = idRegex.search(line).group(1)
-                if modelID in idList:
-                        continue
+                if args.behaviour == 'retrieve':
+                        if modelID not in idList:
+                                continue
+                else:
+                        if modelID in idList:
+                                continue
                 # Put any lines that get here into the out file
                 fileOut.write(line)
+
+# All done
+print('Program completed successfully!')
