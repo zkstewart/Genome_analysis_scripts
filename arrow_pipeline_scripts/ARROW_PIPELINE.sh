da83ba8c28c2e03803384491fcefdbfde2e50a05
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2019-09-17 16:12:33 +1000

    ARROW_PIPELINE resume behaviour
    Code is better able to be configured to resume previously failed runs

--- arrow_pipeline_scripts/ARROW_PIPELINE.sh
+++ arrow_pipeline_scripts/ARROW_PIPELINE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
  
-#PBS -N tel_ARRPIPE
+#PBS -N species_ARRPIPE
 #PBS -l ncpus=1
 #PBS -l walltime=00:01:00
 #PBS -l mem=1G
@@ -9,28 +9,45 @@
 cd $PBS_O_WORKDIR
 
 ## Setup: File prefixes and details that vary each iteration
-ITERATION=2
-GENDIR=/home/n8942188/telmatactis/arrow
-GENNAME=telmatactis_HGAP_blasr_iter.arrow1.fix.fasta
+ITERATION=1
+GENDIR=/home/path/to/genome
+GENNAME=genome_file.fasta
+
+## Setup: Step skipping behaviour
+### Note: Nothing below this line needs to be changed; these skip values should all remain as FALSE unless you intend to resume a job at a step after the first BLASR step
+SKIPBLASR=FALSE
+SKIPSAMSORT=FALSE
+SKIPMERGE=FALSE
+SKIPARROW=FALSE
+SKIPJOBFILE="tmp_file_for_skipping_behaviour_pls_dont_use_this_exact_name.sh"
+echo "
+#!/bin/bash -l
+#PBS -N safe_to_delete_this_file
+#PBS -l walltime=00:00:01
+#PBS -l mem=1mb
+#PBS -l ncpus=1
+
+echo 'ayy lmao'
+" > ${SKIPJOBFILE}
 
 ## Step 1: Run BLASR
 BLASRSCRIPT=run_blasr.sh
 eval "sed -i 's,GENDIR=.*,GENDIR=${GENDIR},' ${BLASRSCRIPT}"
 eval "sed -i 's,GENNAME=.*,GENNAME=${GENNAME},' ${BLASRSCRIPT}"
 eval "sed -i 's,ITERATION=.*,ITERATION=${ITERATION},' ${BLASRSCRIPT}"
-BLASRJOBID=$(qsub ${BLASRSCRIPT})
+if [ "$SKIPBLASR" == "FALSE" ]; then BLASRJOBID=$(qsub ${BLASRSCRIPT}); else BLASRJOBID=$(qsub ${SKIPJOBFILE}); fi
 
 ## Step 2: Run samsort
 SAMSORTSCRIPT=run_samsort.sh
 eval "sed -i 's,ITERATION=.*,ITERATION=${ITERATION},' ${SAMSORTSCRIPT}"
 eval "sed -i 's,#PBS -W depend=afterok.*,#PBS -W depend=afterok:${BLASRJOBID},' ${SAMSORTSCRIPT}"
-SAMSORTJOBID=$(qsub ${SAMSORTSCRIPT})
+if [ "$SKIPSAMSORT" == "FALSE" ]; then SAMSORTJOBID=$(qsub ${SAMSORTSCRIPT}); else SAMSORTJOBID=$(qsub ${SKIPJOBFILE}); fi
 
 ## Step 3: Run mergebam
 MERGEBAMSCRIPT=mergebam.sh
 eval "sed -i 's,ITERATION=.*,ITERATION=${ITERATION},' ${MERGEBAMSCRIPT}"
 eval "sed -i 's,#PBS -W depend=afterok.*,#PBS -W depend=afterok:${SAMSORTJOBID},' ${MERGEBAMSCRIPT}"
-MERGEBAMJOBID=$(qsub ${MERGEBAMSCRIPT})
+if [ "$SKIPMERGE" == "FALSE" ]; then MERGEBAMJOBID=$(qsub ${MERGEBAMSCRIPT}); else MERGEBAMJOBID=$(qsub ${SKIPJOBFILE}); fi
 
 ## Step 4: Run arrow
 ARROWSCRIPT=run_arrow.sh
@@ -38,10 +55,13 @@ eval "sed -i 's,GENDIR=.*,GENDIR=${GENDIR},' ${ARROWSCRIPT}"
 eval "sed -i 's,GENNAME=.*,GENNAME=${GENNAME},' ${ARROWSCRIPT}"
 eval "sed -i 's,ITERATION=.*,ITERATION=${ITERATION},' ${ARROWSCRIPT}"
 eval "sed -i 's,#PBS -W depend=afterok.*,#PBS -W depend=afterok:${MERGEBAMJOBID},' ${ARROWSCRIPT}"
-ARROWJOBID=$(qsub ${ARROWSCRIPT})
+if [ "$SKIPARROW" == "FALSE" ]; then ARROWJOBID=$(qsub ${ARROWSCRIPT}); else ARROWJOBID=$(qsub ${SKIPJOBFILE}); fi
 
 ## Step 5: Fix arrow ID alterations and index with faidx
 FAIDXSCRIPT=fix_and_faidx.sh
 eval "sed -i 's,ITERATION=.*,ITERATION=${ITERATION},' ${FAIDXSCRIPT}"
 eval "sed -i 's,#PBS -W depend=afterok.*,#PBS -W depend=afterok:${ARROWJOBID},' ${FAIDXSCRIPT}"
 qsub ${FAIDXSCRIPT}
+
+## Step 6: Clean up skip job file if relevant
+rm ${SKIPJOBFILE}
