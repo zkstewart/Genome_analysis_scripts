414153b3076f657b8840eec1959ebb54dd99c596
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2019-11-11 16:14:34 +1000

    Create assembly_setup.sh
    Helper script to setup a directory and covert BAM to FASTA for later assembly

---
+++ pipeline_scripts/assembly_pipeline_scripts/assembly_setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+
+#PBS -N assembly_setup
+#PBS -l ncpus=1
+#PBS -l walltime=12:00:00
+#PBS -l mem=5G
+
+# Manual setup
+## Reads files
+READ1=/work/ePGL/pacbio_genome_reads/tubastrea/C1/m54105_180707_015436.subreads.bam
+READ2=/work/ePGL/pacbio_genome_reads/tubastrea/C2/m54105_180707_120628.subreads.bam
+READ3=/work/ePGL/pacbio_genome_reads/tubastrea/C3/m54105_180727_202329.subreads.bam
+READ4=/work/ePGL/pacbio_genome_reads/tubastrea/C4/m54105_180728_063546.subreads.bam
+READ5=/work/ePGL/pacbio_genome_reads/tubastrea/C5/m54105_180728_164913.subreads.bam
+READ6=/work/ePGL/pacbio_genome_reads/tubastrea/C6/m54105_180730_020917.subreads.bam
+READ7=/work/ePGL/pacbio_genome_reads/tubastrea/C7/m54105_180730_121743.subreads.bam
+READ8=/work/ePGL/pacbio_genome_reads/tubastrea/C8/m54105_180730_222745.subreads.bam
+READ9=/work/ePGL/pacbio_genome_reads/tubastrea/coral_extra_cells/cell_1/m54105_181119_031534.subreads.bam
+READ10=/work/ePGL/pacbio_genome_reads/tubastrea/coral_extra_cells/cell_2/m54105_181120_005233.subreads.bam
+READ11=/work/ePGL/pacbio_genome_reads/tubastrea/coral_extra_cells/cell_3/m54105_181120_110608.subreads.bam
+READ12=/work/ePGL/pacbio_genome_reads/tubastrea/coral_extra_cells/cell_4/m54105_181120_211912.subreads.bam
+## Prefixes
+SPECIES=tubastea
+
+# Automatic setup
+HOME_DIR="$PWD"
+FASTA_FILE_NAME=${SPECIES}.subreads.fasta
+
+# STEP 1: Setup working directory
+mkdir $HOME_DIR/tubastrea
+cd $HOME_DIR/tubastrea
+mkdir pacbio_bam_reads
+mkdir pacbio_fasta_reads
+
+# STEP 2: Copy or link to BAM files
+cd $HOME_DIR/pacbio_bam_reads
+## Note: Add or remove $READ# values according to how many values were declared in "Manual setup" above
+### v Optional v: Use symbolic links to save HPC space OR copy the entire files to backup files [Comment out one of thse options with #]
+for r in $READ1 $READ2 $READ3 $READ4 $READ5 $READ6 $READ7 $READ8 $READ9 $READ10 $READ11 $READ12; do ln -s $r .; done # SYMBOLIC LINK OPTION
+#for r in $READ1 $READ2 $READ3 $READ4 $READ5 $READ6 $READ7 $READ8 $READ9 $READ10 $READ11 $READ12; do cp $r .; done # COPY FILE OPTION
+### ^ Optional ^
+
+# STEP 3: Convert BAM to FASTA
+cd $HOME_DIR/pacbio_fasta_reads
+for r in $READ1 $READ2 $READ3 $READ4 $READ5 $READ6 $READ7 $READ8 $READ9 $READ10 $READ11 $READ12; do bamtools convert -in $r -out basename "${r%.*}".fasta -format fasta; done
+for f in *.fasta; do cat $f >> $FASTA_FILE_NAME; rm $f; done
