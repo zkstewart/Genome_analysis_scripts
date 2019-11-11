e7d56ce324ddb7bc817187abae0d713ae0ca94a7
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2019-11-11 21:05:55 +1000

    Fix for fasta_name in canu_assembly.sh

--- pipeline_scripts/assembly_pipeline_scripts/canu_assembly.sh
+++ pipeline_scripts/assembly_pipeline_scripts/canu_assembly.sh
@@ -12,7 +12,7 @@ cd $PBS_O_WORKDIR
 CANU_BIN_DIR=/home/n8942188/various_programs/canu/canu-1.9/Linux-amd64/bin
 ## Reads FASTA location
 FASTA_DIR=/home/n8942188/coral_assembly/tubastrea/pacbio_fasta_reads
-FASTA_NAME=tubastrea_subreads.fasta
+FASTA_NAME=tubastrea.subreads.fasta
 ## Prefixes
 SPECIES=tubastrea
 ## Parameters
