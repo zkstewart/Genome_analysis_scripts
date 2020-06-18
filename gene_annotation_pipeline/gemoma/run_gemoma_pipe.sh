318dfa79dd3b7c12303045a2c4264be4ab152251
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2020-06-18 16:13:18 +1000

    run_gemoma_pipe final
    Fixed to run better

--- gene_annotation_pipeline/gemoma/run_gemoma_pipe.sh
+++ gene_annotation_pipeline/gemoma/run_gemoma_pipe.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
-#PBS -N gem_pipe_clem
-#PBS -l walltime=72:00:00
-#PBS -l mem=100G
-#PBS -l ncpus=12
+#PBS -N gem_pipe_avo
+#PBS -l walltime=00:10:00
+#PBS -l mem=5G
+#PBS -l ncpus=1
 
 cd $PBS_O_WORKDIR
 
@@ -10,13 +10,15 @@ cd $PBS_O_WORKDIR
 module load java/1.8.0_231
 
 ## Setup: Specify file prefixes
-SPECIES=clem
+SPECIES=avo
 
 ###### BELOW THIS LINE ARE "SET ONCE" PARAMETERS, NO NEED TO CHANGE ACROSS RUNS
 
 ## Setup: Manually specify program locations
 GEMOMADIR=/home/n8942188/various_programs/GeMoMa
 GEMOMAJAR=GeMoMa-1.6.4.jar
+
+VARSCRIPTDIR=/home/n8942188/scripts/Various_scripts
 
 ## Setup: Specify input file locations
 SOIDIR=/home/n8942188/plant_annotation/gemoma_related/ids
@@ -25,19 +27,22 @@ REFERENCEDIR=/home/n8942188/plant_annotation/gemoma_related/unzipped_references
 REFERENCESUFFIX=.gff
 
 ## Setup: Specify computational resources
-CPUS=12 # Note: This is only utilised during MMSeqs / BLAST
+CPUS=12 # Note: This is only utilised during MMSeqs / BLAST which is split off as a separate job to this script
 
 ###### NOTHING BELOW THIS LINE NEEDS TO BE CHANGED; OPTIONAL STEP SKIPPING ONLY
 
 ## Setup: Pipeline behaviour specification
-SKIPSTEP2=FALSE ## Note: Set SKIPSTEP2=TRUE if you have already run Step 2 below (genome-specific parts), otherwise leave this as FALSE
-SKIPSTEP3=FALSE ## Note: as above, but for Step 3 below
-
+### Default = TRUE
 RUNMMSEQS=TRUE ## Note: Set RUNMMSEQS=TRUE in most cases for quicker program run, otherwise leave this as FALSE to run tblastn instead
+### Default = FALSE
+SKIPSTEP2=TRUE ## Note: Set SKIPSTEP2=TRUE if you have already run Step 2 below (genome-specific parts), otherwise leave this as FALSE
+SKIPSTEP3=TRUE ## Note: as above, but for Step 3 below
+SKIPEXTRACTOR=TRUE ## Note: Set SKIPEXTRACTOR=FALSE unless you are resuming a run after this point
+SKIPSEARCH=TRUE ## Note: Set SKIPSEARCH=FALSE unless you ... ""
 
 ## Setup: Sub-job qsub values
-STEP4TIME="72:00:00"
-STEP4MEM="70G"
+STEP4TIME="08:00:00"
+STEP4MEM="30G"
 STEP4CPUS=${CPUS}
 
 ## Setup: Auto specify variables
@@ -79,75 +84,87 @@ fi
 ### 3.2: Run createdb / makeblastdb
 if [ "$RUNMMSEQS" == "TRUE" ];
 then
-	mmseqs createdb ${TARGETDIR}/${TARGETFILE} ${WORKDIR}/mmseqs/${SPECIES}.mms2db -v 2;
+	if [ "$SKIPSTEP3" == "FALSE" ];
+	then
+		mmseqs createdb ${TARGETDIR}/${TARGETFILE} ${WORKDIR}/mmseqs/${SPECIES}.mms2db -v 2;
+	fi
 else
-	makeblastdb -out ${WORKDIR}/tblastn/${SPECIES}.blastdb -hash_index -in ${TARGETDIR}/${TARGETFILE} -title "target" -dbtype nucl;
+	if [ "$SKIPSTEP3" == "FALSE" ];
+	then
+		makeblastdb -out ${WORKDIR}/tblastn/${SPECIES}.blastdb -hash_index -in ${TARGETDIR}/${TARGETFILE} -title "target" -dbtype nucl;
+	fi
 fi
 
 ## STEP 4: Run reference-specific pipeline parts
 for file in ${REFERENCEDIR}/*${REFERENCESUFFIX};
 do
-	### 4.1: Job-specific variables
-	TMPNAME=$(basename ${file} ${REFERENCESUFFIX});
-	
-	if [ "$RUNMMSEQS" == "TRUE" ];
-	then
-		SCORE="ReAlign";
-	else
-		SCORE="Trust";
-	fi;
-	
-	### 4.2: Make job file
-	JOBFILE="${OUTDIR}/gemoma_${TMPNAME}.sh"
-	echo "
-	#!/bin/bash -l
-	#PBS -N gemoma_${TMPNAME}
-	#PBS -l walltime=${STEP4TIME}
-	#PBS -l mem=${STEP4MEM}
-	#PBS -l ncpus=${STEP4CPUS}
-	
-	RUNMMSEQS=${RUNMMSEQS}
-	
-	## Step 1: Run Extractor
-	mkdir -p ${WORKDIR}/${TMPNAME}
-	mkdir -p ${WORKDIR}/${TMPNAME}/Extractor
-	java -jar ${GEMOMADIR}/${GEMOMAJAR} CLI Extractor a=${REFERENCEDIR}/${TMPNAME}.gff g=${REFERENCEDIR}/${TMPNAME}.fna p=true Ambiguity=AMBIGUOUS outdir=${WORKDIR}/${TMPNAME}/Extractor
-	
-	## Step 2: Run MMSeqs2 / TBLASTN
-	if [ \"\$RUNMMSEQS\" == \"TRUE\" ];
-	then
-		mmseqs createdb ${WORKDIR}/${TMPNAME}/Extractor/cds-parts.fasta ${WORKDIR}/mmseqs/ref/${TMPNAME}.mms2db;
-		mmseqs search ${WORKDIR}/mmseqs/ref/${TMPNAME}.mms2db ${WORKDIR}/mmseqs/${SPECIES}.mms2db ${WORKDIR}/mmseqs/ref/${TMPNAME}_align.out ${WORKDIR}/mmseqs/ref/${TMPNAME}_tmp \
-		-e 100.0 --threads ${CPUS} -s 8.5 -a --comp-bias-corr 0 --max-seqs 500 --mask 0 --orf-start-mode 1 -v 2;
-		mmseqs convertalis ${WORKDIR}/mmseqs/ref/${TMPNAME}.mms2db ${WORKDIR}/mmseqs/${SPECIES}.mms2db ${WORKDIR}/mmseqs/ref/${TMPNAME}_align.out ${WORKDIR}/${TMPNAME}/search.txt \
-		--format-output \"query,target,pident,alnlen,mismatch,gapopen,qstart,qend,tstart,tend,evalue,bits,empty,raw,nident,empty,empty,empty,qframe,tframe,qaln,taln,qlen,tlen\" -v 2;
-	else
-		tblastn -query ${WORKDIR}/${TMPNAME}/Extractor/cds-parts.fasta -db ${WORKDIR}/tblastn/${SPECIES}.blastdb -evalue 100.0 -out ${WORKDIR}/${TMPNAME}/search.txt -num_threads ${CPUS} -outfmt \
-		\"6 std sallseqid score nident positive gaps ppos qframe sframe qseq sseq qlen slen salltitles\" -db_gencode 1 -matrix BLOSUM62 -seg no -word_size 3 -comp_based_stats F -gapopen 11 -gapextend 1;
-	fi;
-	
-	## Step 3: Run Gene Model Mapper (GeMoMa)
-	
-	### Step 3.1: Setup directory
-	mkdir -p ${WORKDIR}/${TMPNAME}/GeMoMa;
-	
-	### Step 3.2: Run GeMoMa
-	java -jar ${GEMOMADIR}/${GEMOMAJAR} CLI GeMoMa s=${WORKDIR}/${TMPNAME}/search.txt c=${WORKDIR}/${TMPNAME}/Extractor/cds-parts.fasta a=${WORKDIR}/${TMPNAME}/Extractor/assignment.tabular \
-	q=${WORKDIR}/${TMPNAME}/Extractor/proteins.fasta t=${TARGETDIR}/${TARGETFILE} sort=false Score=${SCORE} outdir=${WORKDIR}/${TMPNAME}/GeMoMa i=${WORKDIR}/DenoiseIntrons/denoised_introns.gff \
-	coverage=UNSTRANDED coverage_unstranded=${WORKDIR}/ERE/coverage.bedgraph selected=${SOIDIR}/${TMPNAME}.txt;
-	
-	" > ${JOBFILE}
-	sed -i '1d' ${JOBFILE}
-	
-	### 4.2: Submit job file
-	JOBID=$(qsub ${JOBFILE})
-	
+### 4.1: Job-specific variables
+TMPNAME=$(basename ${file} ${REFERENCESUFFIX});
+
+if [ "$RUNMMSEQS" == "TRUE" ];
+then
+	SCORE="ReAlign";
+else
+	SCORE="Trust";
+fi;
+
+### 4.2: Make job file
+JOBFILE="${OUTDIR}/gemoma_${TMPNAME}.sh"
+echo "
+#!/bin/bash -l
+#PBS -N gemoma_${TMPNAME}
+#PBS -l walltime=${STEP4TIME}
+#PBS -l mem=${STEP4MEM}
+#PBS -l ncpus=${STEP4CPUS}
+
+cd ${HOMEDIR}
+RUNMMSEQS=${RUNMMSEQS}
+source /home/n8942188/.bashrc
+module load java/1.8.0_231
+
+## Step 1: Run Extractor
+mkdir -p ${WORKDIR}/${TMPNAME}
+mkdir -p ${WORKDIR}/${TMPNAME}/Extractor
+
+if [ "$SKIPEXTRACTOR" == "FALSE" ];
+then
+java -jar ${GEMOMADIR}/${GEMOMAJAR} CLI Extractor a=${REFERENCEDIR}/${TMPNAME}.gff g=${REFERENCEDIR}/${TMPNAME}.fna p=true Ambiguity=AMBIGUOUS outdir=${WORKDIR}/${TMPNAME}/Extractor;
+fi;
+
+## Step 2: Run MMSeqs2 / TBLASTN
+if [ \"\$RUNMMSEQS\" == \"TRUE\" ];
+then
+if [ "$SKIPSEARCH" == "FALSE" ];
+then
+mmseqs createdb ${WORKDIR}/${TMPNAME}/Extractor/cds-parts.fasta ${WORKDIR}/mmseqs/ref/${TMPNAME}.mms2db;
+mmseqs search ${WORKDIR}/mmseqs/ref/${TMPNAME}.mms2db ${WORKDIR}/mmseqs/${SPECIES}.mms2db ${WORKDIR}/mmseqs/ref/${TMPNAME}_align.out ${WORKDIR}/mmseqs/ref/${TMPNAME}_tmp \
+-e 100.0 --threads ${CPUS} -s 8.5 -a --comp-bias-corr 0 --max-seqs 500 --mask 0 --orf-start-mode 1 -v 2;
+mmseqs convertalis ${WORKDIR}/mmseqs/ref/${TMPNAME}.mms2db ${WORKDIR}/mmseqs/${SPECIES}.mms2db ${WORKDIR}/mmseqs/ref/${TMPNAME}_align.out ${WORKDIR}/${TMPNAME}/search.unsorted.txt \
+--format-output \"query,target,pident,alnlen,mismatch,gapopen,qstart,qend,tstart,tend,evalue,bits,empty,raw,nident,empty,empty,empty,qframe,tframe,qaln,taln,qlen,tlen\" -v 2;
+python ${VARSCRIPTDIR}/mmseqs2_postprocess.py -i ${WORKDIR}/${TMPNAME}/search.unsorted.txt -o ${WORKDIR}/${TMPNAME}/search.txt --recompute_fasta ${WORKDIR}/${TMPNAME}/Extractor/cds-parts.fasta
+fi;
+else
+if [ "$SKIPSEARCH" == "FALSE" ];
+then
+tblastn -query ${WORKDIR}/${TMPNAME}/Extractor/cds-parts.fasta -db ${WORKDIR}/tblastn/${SPECIES}.blastdb -evalue 100.0 -out ${WORKDIR}/${TMPNAME}/search.txt -num_threads ${CPUS} -outfmt \
+\"6 std sallseqid score nident positive gaps ppos qframe sframe qseq sseq qlen slen salltitles\" -db_gencode 1 -matrix BLOSUM62 -seg no -word_size 3 -comp_based_stats F -gapopen 11 -gapextend 1;
+fi;
+fi;
+
+## Step 3: Run Gene Model Mapper (GeMoMa)
+
+### Step 3.1: Setup directory
+mkdir -p ${WORKDIR}/${TMPNAME}/GeMoMa;
+
+### Step 3.2: Run GeMoMa
+java -jar ${GEMOMADIR}/${GEMOMAJAR} CLI GeMoMa s=${WORKDIR}/${TMPNAME}/search.txt c=${WORKDIR}/${TMPNAME}/Extractor/cds-parts.fasta a=${WORKDIR}/${TMPNAME}/Extractor/assignment.tabular \
+q=${WORKDIR}/${TMPNAME}/Extractor/proteins.fasta t=${TARGETDIR}/${TARGETFILE} sort=false Score=${SCORE} outdir=${WORKDIR}/${TMPNAME}/GeMoMa i=${WORKDIR}/DenoiseIntrons/denoised_introns.gff \
+coverage=UNSTRANDED coverage_unstranded=${WORKDIR}/ERE/coverage.bedgraph selected=${SOIDIR}/${TMPNAME}.txt;
+
+" > ${JOBFILE}
+sed -i '1d' ${JOBFILE}
+
+### 4.2: Submit job file
+qsub ${JOBFILE}
+
 done
-
-## STEP 5: Run combining pipeline parts
-### 5.1: Run GeMoMa Annotation Filter (GAF)
-#mkdir -p ${WORKDIR}/${TMPNAME}/GAF;
-#java -jar ${GEMOMADIR}/${GEMOMAJAR} CLI GAF g=${WORKDIR}/${TMPNAME}/GeMoMa/predicted_annotation.gff outdir=${WORKDIR}/${TMPNAME}/GAF;
-
-### 5.2: Run AnnotationFinalizer
-#mkdir -p ${WORKDIR}/${TMPNAME}/AnnotationFinalizer;
